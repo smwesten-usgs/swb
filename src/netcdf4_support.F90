3cc29980ef24718452f07980f5974a618cede28f
Steve Westenbroek <smwesten@usgs.gov>
2013-05-15 13:17:16 -0500

    Minor changes

--- src/netcdf4_support.F90
+++ src/netcdf4_support.F90
@@ -715,7 +715,7 @@ subroutine netcdf_open_file(NCFILE, sFilename, iLU)
   call echolog("Attempting to open READONLY NetCDF file: " &
     //dquote(sFilename))
 
-  call nc_trap( nc_open(sFilename, &
+  call nc_trap( nc_open(trim(sFilename)//c_null_char, &
                 NC_READONLY, NCFILE%iNCID), __FILE__, __LINE__ )
 
   call nc_trap( nc_inq_format(ncid=NCFILE%iNCID, formatp=NCFILE%iFileFormat), &
